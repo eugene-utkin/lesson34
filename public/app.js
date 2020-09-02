dcca715d93c1a1298395ed1540ee040735e950e3
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-02 15:24:43 +0500

    cart_get_orders added

--- public/app.js
+++ public/app.js
@@ -36,3 +36,19 @@ function cart_get_number_of_items()
 		}
 		return cnt;
 }
+
+function cart_get_orders()
+{
+	var orders = '';
+	for(var i = 0; i < window.localStorage.length; i++)
+		{
+			var key = window.localStorage.key(i);
+			var value = window.localStorage.getItem(key);
+
+			if(key.indexOf('product_') == 0)
+			{
+				orders = orders + key + '=' + value + ',';
+			}
+		}
+		return cnt;
+}
